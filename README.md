e90665315e9abbe4ce247012ec9680fbfb436d83
Md. Samiur Rahman Mukul <sr.mukul9090@gmail.com>
2021-04-06 08:36:45 +0600

    React Font-End-Dev Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# React-E-Commerse-Project-Mobile-Shop
-Hey, this is full functional E-commerce  project. This application font end development React (JavaScript library). Thanks
+# React E-Commerce Project - Mobile Shop
---
+++ react-mobile-shop/src/App/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+// * import module
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// * import assets
+// import Logo from "./../Assets/logo/phone-logo.svg";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <i className="fas fa-home" />
+        </div>
+        <div className="col-6">column number two</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
